e0168a961786978c3f773eef0650e677e0290dc1
Paulina Ciach <paulina.ciach95@gmail.com>
2022-02-02 00:32:12 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Footer from './PortfolioContainer/Home/Footer/Footer';
-import Profile from './PortfolioContainer/Home/Profile';
-
+import PortfolioContainer from './PortfolioContainer/PortfolioContainer'
 
 function App() {
   return (
     <div className="App">
-        <Profile/>
-        <Footer/>
+        <PortfolioContainer/>
     </div>
   );
 }
